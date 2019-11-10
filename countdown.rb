bb2d03c4cfc308942a6d08a06f8a237c9a4edc48
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-10 23:48:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,10 +6,11 @@ def countdown (number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep (number)
+def countdown (number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
+    sleep (5)
   end
   return "HAPPY NEW YEAR!"
 end
